cf8d5431156c22816230a334ea23b2d0768ebc40
VIVEK BHANDARI <ervivekbhandari@gmail.com>
2023-04-18 00:15:06 -0700

    - New input system added - Basic collision with counters added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,44 +7,72 @@ public class Player : MonoBehaviour
     public delegate void Movement(bool _value);
     public event Movement Event_OnPlayerMove;
 
+    GameInput gameInput;
 
-
-    [SerializeField]
+    [SerializeField] 
     byte moveSpeed = 7;
+    byte rotationSpeed = 15;
+
+    Vector3 lastInteractDirection;
+
+    [SerializeField] 
+    LayerMask counterLayerMask;
 
 
-
+    private void Start()
+    {
+        gameInput = FindObjectOfType<GameInput>();
+    }
 
     private void Update()
     {
-
-        Vector2 inputVector = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1; 
-        }
-        else if (Input.GetKey(KeyCode.S)) 
-        { 
-            inputVector.y = -1; 
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        inputVector.Normalize();
-        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
-
-        bool response = inputVector == Vector2.zero ? false : true;
-        Event_OnPlayerMove?.Invoke(response);
+        HandleMovement();
+        HandleInteractions();
     }
 
 
+    private void HandleInteractions()
+    {
+        // for input movement
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
 
+        lastInteractDirection = moveDirection == Vector3.zero ? lastInteractDirection : moveDirection;
 
+        byte interactDistance = 2;
+        if(Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit hit, interactDistance, counterLayerMask))
+        {
+            if(hit.transform.parent.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+
+        }
+
+    }
+
+    private void HandleMovement()
+    {
+        // for input movement
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized(); 
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+
+        byte playerHeight = 2;
+        float playerRadius = 0.7f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        // collision check
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+
+        // animation control
+        bool response = inputVector == Vector2.zero ? false : true;
+        Event_OnPlayerMove?.Invoke(response);
+
+        // rotation control
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+    }
 }
